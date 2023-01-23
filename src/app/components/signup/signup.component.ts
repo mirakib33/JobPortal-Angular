fbcf2e673867ea6df08badd32343c31ff8a90bd2
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-01-23 13:51:39 +0600

    signup form data submitted

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SignupService } from 'src/app/services/signup.service';
 
 @Component({
@@ -7,34 +7,27 @@ import { SignupService } from 'src/app/services/signup.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
   constructor(private signupService:SignupService) { }
 
   form!: FormGroup;
   // signup!:Signup[];
 
-  // ngOnInit(): void {
-  //   this.form = new FormGroup({
-  //     title: new FormControl('', [Validators.required]),
-  //     body: new FormControl('', Validators.required)
-  //     ,
-  //     firstName:[''],
-  //     lastName:[''],
-  //     email:[''],
-  //     phone:[''],
-  //     password:[''],
-  //     userType:[''],
-  //   });
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+      password: new FormControl(''),
+      userType: new FormControl(''),
+    });
 
-  //   this.signupService.getSignup().subscribe((data:Signup[]) => {
-  //     this.signup = data;
-  //   });
-  // }
+  }
 
   submit(){
     this.signupService.save(this.form.value).subscribe((res:any) => {
-      console.log('Post created successfully!');
     })
   }
 }
