7ab222292f418f6245b4c5d79cf049fbda62d573
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-02-07 20:32:07 +0600

    Revert "join table"
    This reverts commit baba9658e707b2a730297d7ce015e04b46fdf889.

--- src/app/components/applicant/applied-jobs/per-applied-jobs/per-applied-jobs.component.ts
+++ src/app/components/applicant/applied-jobs/per-applied-jobs/per-applied-jobs.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { PerJobCv } from 'src/app/models/employer/per-job-cv.model';
-import { PerJobCvService } from 'src/app/services/employer/per-job-cv.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-per-applied-jobs',
   templateUrl: './per-applied-jobs.component.html',
   styleUrls: ['./per-applied-jobs.component.scss']
 })
-export class PerAppliedJobsComponent implements OnInit {
-
-  userId: number = 33;
-  perJobCv!: PerJobCv;
-
-  constructor(private formBuilder:FormBuilder, private perJobCvService:PerJobCvService) {}
-
-  ngOnInit(): void {
-    this.perJobCvService.find(this.userId).subscribe((data: PerJobCv)=>{
-      this.perJobCv = data;
-    });
-  }
+export class PerAppliedJobsComponent {
 
 }
--- src/app/models/employer/per-job-cv.model.ts
+++ src/app/models/employer/per-job-cv.model.ts
@@ -1,9 +1,6 @@
-import { PerJobPost } from "./per-job-post.model";
-
 export class PerJobCv {
     id?: number;
     userId!: number;
     jobId!: number;
     status!: boolean;
-    perJobPost!: PerJobPost;
 }
