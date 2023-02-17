3a2944ddb1829493cde1e830fe2d5a33b158a98c
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-02-18 03:12:31 +0600

    Authentication

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -8,24 +8,63 @@ import { Signup } from '../models/signup.model';
 })
 export class SignupService {
 
-  constructor(private httpClient: HttpClient) { }
-
   private url = "http://localhost:8080";
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
+  requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
+
+  constructor(
+    private httpclient: HttpClient,
+    private userAuthService: UserAuthService
+  ) {}
+
+  public login(loginData) {
+    return this.httpclient.post(this.url + '/signin', loginData, {
+      headers: this.requestHeader,
+    });
   }
 
-  save(signup:Signup): Observable<any> {
-  
-    return this.httpClient.post(this.url + '/signup', JSON.stringify(signup), this.httpOptions)
-  
-    .pipe(
-      catchError(this.errorHandler)
-    )
+  public forUser() {
+    return this.httpclient.get(this.url + '/forUser', {
+      responseType: 'text',
+    });
   }
+
+
+  public forAdmin() {
+    return this.httpclient.get(this.url + '/forAdmin', {
+      responseType: 'text',
+    });
+  }
+
+  public roleMatch(allowedRoles): boolean {
+    let isMatch = false;
+    const userRoles: any = this.userAuthService.getRoles();
+
+    if (userRoles != null && userRoles) {
+      for (let i = 0; i < userRoles.length; i++) {
+        for (let j = 0; j < allowedRoles.length; j++) {
+          if (userRoles[i].roleName === allowedRoles[j]) {
+            isMatch = true;
+            return isMatch;
+          } else {
+            return isMatch;
+          }
+        }
+      }
+    }
+  }
+
+
+
+
+  // save(signup:Signup): Observable<any> {
+  
+  //   return this.httpClient.post(this.url + '/signup', JSON.stringify(signup), this.httpOptions)
+  
+  //   .pipe(
+  //     catchError(this.errorHandler)
+  //   )
+  // }
 
   errorHandler(error:any) {
     let errorMessage = '';
