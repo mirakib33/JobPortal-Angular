f4b959ca58814be7091aeb39bdd23a050a2ce9a1
Mazharul Islam Rakib <mirakib33@gmail.com>
2023-02-19 20:19:35 +0600

    Revert "Authentication"
    This reverts commit 3a2944ddb1829493cde1e830fe2d5a33b158a98c.

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -8,63 +8,24 @@ import { Signup } from '../models/signup.model';
 })
 export class SignupService {
 
+  constructor(private httpClient: HttpClient) { }
+
   private url = "http://localhost:8080";
 
-  requestHeader = new HttpHeaders({ 'No-Auth': 'True' });
-
-  constructor(
-    private httpclient: HttpClient,
-    private userAuthService: UserAuthService
-  ) {}
-
-  public login(loginData) {
-    return this.httpclient.post(this.url + '/signin', loginData, {
-      headers: this.requestHeader,
-    });
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
   }
 
-  public forUser() {
-    return this.httpclient.get(this.url + '/forUser', {
-      responseType: 'text',
-    });
-  }
-
-
-  public forAdmin() {
-    return this.httpclient.get(this.url + '/forAdmin', {
-      responseType: 'text',
-    });
-  }
-
-  public roleMatch(allowedRoles): boolean {
-    let isMatch = false;
-    const userRoles: any = this.userAuthService.getRoles();
-
-    if (userRoles != null && userRoles) {
-      for (let i = 0; i < userRoles.length; i++) {
-        for (let j = 0; j < allowedRoles.length; j++) {
-          if (userRoles[i].roleName === allowedRoles[j]) {
-            isMatch = true;
-            return isMatch;
-          } else {
-            return isMatch;
-          }
-        }
-      }
-    }
-  }
-
-
-
-
-  // save(signup:Signup): Observable<any> {
+  save(signup:Signup): Observable<any> {
   
-  //   return this.httpClient.post(this.url + '/signup', JSON.stringify(signup), this.httpOptions)
+    return this.httpClient.post(this.url + '/signup', JSON.stringify(signup), this.httpOptions)
   
-  //   .pipe(
-  //     catchError(this.errorHandler)
-  //   )
-  // }
+    .pipe(
+      catchError(this.errorHandler)
+    )
+  }
 
   errorHandler(error:any) {
     let errorMessage = '';
